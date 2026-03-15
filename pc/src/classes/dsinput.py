680ab6af8add6301bdf7fb1298262c2135a0fbe3
michaeljwalla <michaeljwalla@gmail.com>
2026-03-14 19:53:27 -0500

    finalize (hopefully) general input classes and helpers

--- pc/src/classes/dsinput.py
+++ pc/src/classes/dsinput.py
@@ -1,8 +1,21 @@
-from abc import ABCMeta
-from enum import Enum, EnumMeta
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Callable, Any
+from time import perf_counter as tick
 
-class DSInput(ABCMeta, EnumMeta):
-    pass
-
-class DSTouch(DSInput, Enum):
-    pass
+# no explicitly hashing these (use is for equality)
+# as usually one instantiation gets updated to save memory
+class DSInput(ABC):
+    def __eq__(self, other): return type(self) == type(other)
+    @abstractmethod
+    def clone(self):
+        pass
+#
+@dataclass(slots=True, eq=False)
+class DSTouchpad(DSInput):
+    touch: bool = False
+    x: float = 0
+    y: float = 0
+    
+    def clone(self):
+        return DSTouchpad(self.touch, self.x, self.y)
--- pc/src/classes/inputdevice.py
+++ pc/src/classes/inputdevice.py
@@ -1,5 +1,45 @@
-from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from .dsinput import *
 
-class InputDevice(ABC):
-    __slots__ = tuple()
-    pass
+@dataclass(slots=True, eq=False)
+class InputBinding:
+    input: DSInput
+    callback: Callable # args i: DSInput, dt: float (s). first dt is always 0
+    enabled: bool = False
+    _lasttick: int = 0
+
+    def __hash__(self):
+        return hash(type(self.input), self.callback) # because DSInput might be cloned, but callback should always be by reference
+    #
+    def update(self):
+        self.callback(self.input, 0 if not _lasttick else tick() - _lasttick) 
+        _lasttick = tick()
+
+@dataclass(slots=True)
+class InputDevice:
+    name: str = "InputDevice"
+    registers: dict[type[DSInput], set[InputBinding]] = field(default_factory=dict)
+
+    def add(self, i: InputBinding):
+        t = type(i.input)
+        if t not in self.registers:
+            self.registers[t] = set(i)
+            return
+        #
+        register = self.registers[t]
+        if i in register: return
+        #
+        register.add(i)
+        return
+    
+    def remove(self, i:InputBinding):
+        t = type(i.input)
+        if t not in self.registers:
+            return
+        #
+        register = self.registers[t]
+        if i not in register: return
+        #
+        register.remove(i)
+        return
+
