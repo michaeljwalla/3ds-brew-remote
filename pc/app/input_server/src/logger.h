9a8d3ca8ce0c5a7c7bf2d98aab3ef57222ec7e83
michaeljwalla <michaeljwalla@gmail.com>
2026-03-27 14:57:21 -0500

    thread-safe objects and singleton

--- pc/app/input_server/src/logger.h
+++ pc/app/input_server/src/logger.h
@@ -1,16 +1,24 @@
 #include <functional>
+#include <mutex>
+#include <shared_mutex>
 #include <string_view>
 #include <variant>
 #include <string>
 #include <iostream>
+
+//this unifies how you can log stuff. passes info through << and thats about it for now.
+//set any callback but you may have to redefine operator<< for specific types for complex tasks
 
 class LoggerProxy {
 public:
     using LogArg = std::variant<int, float, bool, std::string, std::string_view, const char*, 
                                std::ostream& (*)(std::ostream&)>;
     using LogFunc = std::function<void(const LogArg&)>;
-private:
+
+protected:
     LogFunc _callback;
+
+private:
     
     static void default_log(const LogArg& arg) { //defaults to cout << ... functionality
         std::visit(
@@ -20,18 +28,36 @@ private:
         return;
     }
 public:
-    static LoggerProxy& singleton() {
-        static LoggerProxy inst;
-        return inst;
-    }
+    static LoggerProxy& singleton();
     LoggerProxy() : _callback(default_log) {}
     explicit LoggerProxy(const LogFunc callback): _callback(callback) {}
 
-    LoggerProxy& operator<<(const LogArg& arg) {
+    virtual LoggerProxy& operator<<(const LogArg& arg) {
         _callback(arg);
         return *this;
     }
-    void set_logger(const LogFunc callback) {
+    virtual void set_logger(const LogFunc callback) {
         this->_callback = callback;
     }
+    virtual ~LoggerProxy() = default;
 };
+
+class ThreadSafeLoggerProxy : public LoggerProxy {
+    mutable std::shared_mutex _mutex; //default constructed
+public:
+    ThreadSafeLoggerProxy& operator<<(const LogArg& arg) override {
+        std::shared_lock lock(_mutex);
+        LoggerProxy::operator<<(arg);
+        return *this;
+    }
+    void set_logger(const LogFunc callback) override {
+        std::unique_lock lock(_mutex);
+        LoggerProxy::set_logger(callback);
+    }
+};
+
+//define singleton
+static LoggerProxy singleton()  {
+    static ThreadSafeLoggerProxy inst;
+    return inst;
+}
