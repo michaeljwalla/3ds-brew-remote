b42d5db99fb2ec0d64ffce142e2abebfba781355
michaeljwalla <michaeljwalla@gmail.com>
2026-04-30 23:07:56 -0500

    InputController will manage live devices

--- pc/app/input_server/src/host/host_environment.h
+++ pc/app/input_server/src/host/host_environment.h
@@ -1,27 +1,42 @@
 #pragma once
 #include <memory>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 #include <string>
 #include "../server/logger.h"
 
 class InputObject{
-    private:
-        static size_t object_counter;
-        const size_t id;
-        const std::string name;
     public:
-    InputObject(): id(object_counter++), name{static_cast<char>(id)} {
-        return;
-    }
-    InputObject(std::string_view name): id(object_counter++), name{name} {
+        using ObjectID = uint32_t; //if you have over 2 bil input devices wyd
+        using ObjectName = std::string;
+    private:
+        const ObjectID id;
+        const ObjectName name;
+        //non-const unknown object*(?) = nullptr (?);
+        InputObject(ObjectID id, std::string_view name): id{id}, name{name} {}
 
-    }
-    const std::string& getName() const {
-        return this->name;
-    }
+        //migrate (explicit to reconstruct with InputObject& to steal)
+    public:
+        // must use input controller
+        InputObject() = delete;
+        InputObject(const InputObject&) = delete;
+        InputObject(InputObject&&) = delete;
+        InputObject& operator=(const InputObject&) = delete;
+        InputObject& operator=(InputObject&&) = delete;
+
+        const ObjectName& getName() const {
+            return this->name;
+        }
+        ObjectID getID() const {
+            return this->id;
+        }
+    
+    friend class InputController;
     friend std::ostream& operator<<(std::ostream&, const InputObject&);
     friend Logger& operator<<(Logger&, const InputObject&);
 };
+
 inline std::ostream& operator<<(std::ostream& os, const InputObject& i) {
     os << "InputObject " << i.name;
     return os;
@@ -29,9 +44,75 @@ inline std::ostream& operator<<(std::ostream& os, const InputObject& i) {
 inline Logger& operator<<(Logger& log, const InputObject& i) {
     log << "InputObject " << i.name;
     return log;
-}
+};
+//
 
-InputObject* spawn_device();
-bool register_device( std::unique_ptr<InputObject> );
-std::vector<InputObject*> get_devices();
+class InputController {
+    public:
+        using ObjectID = InputObject::ObjectID;
+        using ObjectName = InputObject::ObjectName;
+        using uptr = std::unique_ptr<InputObject>;
+    private:
+        std::unordered_map<ObjectID, uptr> inputs;
+        ObjectID counter;
+        Logger* logger; //non owning
 
+        //factory
+        uptr create(ObjectID id, std::string_view name) {
+            return uptr(new InputObject(id, name));
+        }
+    public:
+        InputController(): inputs{}, counter{0}, logger{nullptr} {}
+        InputController(Logger* logger): inputs{}, counter{0}, logger{logger} {}
+        ~InputController() = default;
+
+        //no copy
+        InputController(const InputController&) = delete;
+        InputController& operator=(const InputController&) = delete;
+
+        //move operations
+        InputController(InputController&& other) noexcept:
+            inputs{ std::move(other.inputs) },
+            counter{ std::exchange(other.counter, 0) },
+            logger{ std::exchange(other.logger, nullptr) }
+            {}
+        InputController& operator=(InputController&& other) noexcept {
+            if (this == &other) return *this;
+            this->inputs = std::move(other.inputs);
+            this->counter =  std::exchange(other.counter, 0);
+            this->logger = std::exchange( other.logger, nullptr );
+            return *this;
+        }
+        //
+
+        ObjectID spawn() {
+            auto id = counter++;
+            inputs.emplace(id, create(id, std::to_string(id)));
+            return id;
+        }
+        ObjectID spawn(std::string_view name) {
+            auto id = counter++;
+            inputs.emplace(id, create(id, name));
+            return id;
+        }
+        
+        //
+        bool include(uptr&& in) {
+            //migrate() here
+            return false;
+        }
+
+        size_t size() const {
+            return inputs.size();
+        }
+
+        std::vector<InputObject*> get_objects() const {
+            std::vector<InputObject*> out;
+            out.reserve( inputs.size() );
+            for (auto& [id, ptr]: inputs) {
+                out.push_back( ptr.get() );
+            }
+            return out;
+        }
+        
+};
