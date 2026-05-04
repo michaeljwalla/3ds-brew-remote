84c8b20c63241ba68e0a47d39a50fcf9ef697105
michaeljwalla <michaeljwalla@gmail.com>
2026-05-04 16:46:08 -0500

    input()

--- pc/app/py_poc/main.py
+++ pc/app/py_poc/main.py
@@ -2,7 +2,7 @@ import time
 import src.receiver as receiver
 
 receiver.set_timeline(print)
-receiver.start("127.0.0.1", displayLive=True)
+receiver.start(input("address: ") or "127.0.0.1", displayLive=True)
 
 time.sleep(5)
 receiver.stop()
