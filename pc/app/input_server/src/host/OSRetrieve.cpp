ab68657b7b1841e8276acf8ba557eaa365b329af
michaeljwalla <michaeljwalla@gmail.com>
2026-05-06 20:50:20 -0500

    outsource device input-spawners (foreshadowing)

---
+++ pc/app/input_server/src/host/OSRetrieve.cpp
@@ -0,0 +1,27 @@
+
+#include <linux/uinput.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cerrno>
+#include <system_error>
+#include <sys/inotify.h>
+#include <sys/stat.h>
+#include <poll.h>
+#include <dirent.h>
+
+//return fd input referrer
+int os_spawn() {
+    int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    if (fd < 0)
+        throw std::system_error(errno, std::generic_category(),
+            "open /dev/uinput (need root or input group)");
+    return fd;
+}
+
+void os_close(int fd) {
+    if (fd < 0) return;
+
+    ioctl(fd, UI_DEV_DESTROY);
+    close(fd);
+    return;
+}
---
+++ pc/app/input_server/src/host/OSRetrieve.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int os_spawn();
+void os_close(int fd);
