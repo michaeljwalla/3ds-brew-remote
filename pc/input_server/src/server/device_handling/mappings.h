ca929951f93597b6f8970dfce32b966e378850ea
michaeljwalla <michaeljwalla@gmail.com>
2026-05-10 11:03:30 -0500

    changed handler in-param to reference

--- pc/input_server/src/server/device_handling/mappings.h
+++ pc/input_server/src/server/device_handling/mappings.h
@@ -12,7 +12,7 @@
 namespace {
     using HandlerReturnType = int;
     template<typename T>
-    using Handler = HandlerReturnType(*)(T in, RawInput code);
+    using Handler = HandlerReturnType(*)(T& in, RawInput code);
     //
     template<typename T>
     using UntypedKeymap = std::unordered_map<ButtonMask, Handler<T>>;
@@ -43,12 +43,12 @@ class InputMap { //immutable
         const std::string& getName() const { return name; }
         const Keymap& getKeymap() const { return keymap; }
         //
-        HandlerReturnType handle(T input, ButtonMask btn, RawInput& code) const {
+        HandlerReturnType handle(T& input, ButtonMask btn, RawInput& code) const {
             auto it = keymap.find(btn);
             if (it == keymap.end()) return -1;
             return it->second(input, code);
         }
-        vector<HandlerReturnType> handleAll(T input, RawInput& code) const {
+        vector<HandlerReturnType> handleAll(T& input, RawInput& code) const {
             vector<HandlerReturnType> results(NUM_INPUTS);
             for (size_t i = 0; i < NUM_INPUTS; ++i) {
                 const ButtonMask btn = static_cast<ButtonMask>(1 << i);
