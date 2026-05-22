e99480fddae06368e9f817ba65b87e3ec66ee7a2
michaeljwalla <michaeljwalla@gmail.com>
2026-05-21 23:10:25 -0500

    InputGamepad implementation (hope SDL picks it up)

--- pc/input_server/src/server/device_handling/statics/Objects.cpp
+++ pc/input_server/src/server/device_handling/statics/Objects.cpp
@@ -68,3 +68,76 @@ void InputMouse::sync(bool force) {
 InputMouse::coordinate InputMouse::get_pos() const{
     return pos;
 }
+//
+
+//InputGamepad
+InputGamepad::InputGamepad(ObjectID id, std::string_view name):
+    InputObject(id, name, InputTypes::GAMEPAD)
+{}
+void InputGamepad::init() {
+    mapping = &get_mapping(InputTypes::GAMEPAD);
+    os.spawn();
+
+    os.enableKey(BTN_A);
+    os.enableKey(BTN_B);
+    os.enableKey(BTN_X);
+    os.enableKey(BTN_Y);
+    os.enableKey(BTN_TL);
+    os.enableKey(BTN_TR);
+    os.enableKey(BTN_SELECT);
+    os.enableKey(BTN_START);
+    os.enableKey(BTN_MODE);
+    os.enableKey(BTN_THUMBL);
+    os.enableKey(BTN_THUMBR);
+
+    input_absinfo stick    { 0, -32768, 32767, 16, 128, 0 };
+    input_absinfo trigger  { 0,      0,   255,  0,   0, 0 };
+    input_absinfo hat      { 0,     -1,     1,  0,   0, 0 };
+
+    os.enableAbsAxis(ABS_X,     stick);
+    os.enableAbsAxis(ABS_Y,     stick);
+    os.enableAbsAxis(ABS_Z,     trigger);
+    os.enableAbsAxis(ABS_RX,    stick);
+    os.enableAbsAxis(ABS_RY,    stick);
+    os.enableAbsAxis(ABS_RZ,    trigger);
+    os.enableAbsAxis(ABS_HAT0X, hat);
+    os.enableAbsAxis(ABS_HAT0Y, hat);
+
+    os.create("Microsoft X-Box 360 pad", 0x045e, 0x028e, 0x0110);
+    return;
+}
+
+void InputGamepad::button_down(int btn) {
+    os.emit(EV_KEY, btn, 1);
+}
+void InputGamepad::button_up(int btn) {
+    os.emit(EV_KEY, btn, 0);
+}
+void InputGamepad::button_click(int btn) {
+    os.emit(EV_KEY, btn, 1);
+    os.sync(true);
+    os.emit(EV_KEY, btn, 0);
+    os.sync(true);
+}
+void InputGamepad::sync(bool force) {
+    os.sync(force);
+}
+
+static int snap(float v) {
+    return v > 0.0f ? 1 : (v < 0.0f ? -1 : 0);
+}
+
+void InputGamepad::set_joystick(Side which, coords<float> dir) {
+    int axisX = (which == Side::LEFT) ? ABS_X  : ABS_RX;
+    int axisY = (which == Side::LEFT) ? ABS_Y  : ABS_RY;
+    os.emit(EV_ABS, axisX, static_cast<int>(dir.x * 32767));
+    os.emit(EV_ABS, axisY, static_cast<int>(dir.y * 32767));
+}
+void InputGamepad::set_trigger(Side which, float amount) {
+    int axis = (which == Side::LEFT) ? ABS_Z : ABS_RZ;
+    os.emit(EV_ABS, axis, static_cast<int>(amount * 255));
+}
+void InputGamepad::set_dpad(coords<float> dir) {
+    os.emit(EV_ABS, ABS_HAT0X, snap(dir.x));
+    os.emit(EV_ABS, ABS_HAT0Y, snap(dir.y));
+}
--- pc/input_server/src/server/device_handling/statics/Objects.h
+++ pc/input_server/src/server/device_handling/statics/Objects.h
@@ -29,3 +29,30 @@ class InputMouse: public InputObject {
     friend class InputController;
 
 };
+
+class InputGamepad: public InputObject {
+    InputGamepad(ObjectID id, std::string_view name);
+
+    void init() override;
+    public:
+        enum class Side {
+            LEFT, RIGHT
+        };
+        void button_down(int btn);
+        void button_up(int btn);
+        // this fires syncs immediately (others do not)
+        // since button down/up have to be in different batches to register properly
+        void button_click(int btn);
+        // dir components in [-1.0, 1.0]
+        void set_joystick(Side which, coords<float> dir);
+        // dir components snapped to {-1, 0, 1}
+        void set_dpad(coords<float> dir);
+        // amount in [0.0, 1.0]
+        void set_trigger(Side which, float amount);
+
+        // flush
+        void sync(bool force = false);
+
+    friend class InputController;
+
+};
